ecafbb750739fdb74dcc85ff714c98cc201b12eb
zhaisilong <zhaisilong@outlook.com>
2022-07-07 11:01:58 +0800

    log

--- pretrain.sh
+++ pretrain.sh
@@ -1,5 +1,7 @@
+DIR="/hy-tmp"
+
 python pretrain.py \
-  --data_file='/home/seeyou/data/datasets/glue/MRPC/train.tsv' \
-  --vocab='/home/seeyou/data/models/huggingface/bert-base-uncased/vocab.txt' \
+  --data_file="$DIR/datasets/MRPC/train.tsv" \
+  --vocab="$DIR/models/bert-base-uncased/vocab.txt" \
   --save_dir='models' \
   --log_dir='logs'
